8e2dfc76241887bec219f8341767ae2ce740f9d8
Erindcl <cailian_erin@163.com>
2019-03-07 20:32:15 +0800

    完成首页部分布局

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,9 +5,8 @@ import * as home  from "./aciton";
 import moment from "moment";
 moment.locale("zh-cn");
 import "./style.scss";
-import { WordChart, BarChart, LineChart, ScatterChart, PieChart, MapChart } from 'components/charts';
-import { barOption, lineOption, mapOption, scatterOption, pieOption, wordOption } from 'constants/option';
-import { Chart, Geom, Axis, Tooltip } from "bizcharts";
+
+import { Button, Divider, Row, Col } from 'antd';
 
 @connect(
   state => ({ ...state.home }),
@@ -17,90 +16,58 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      barOption:barOption,
-      config:{}
+      
     };
   }
   componentDidMount() {
     this.props.getHomeData({});
-    this.setChart();
   }
- setChart=()=>{
-  this.setState({barOption,
-    config:{
-    height:'301px',
-    handle:this.clickBar
-  }})
- }
-  componentWillReceiveProps(nextProps) {
-
-  }
-  clickBar=(data)=>{
-    console.log('点击触发啦',data)
-  }
+  
   render() {
-    const {barOption,config}=this.state;
-    const data = [
-      {
-        year: "1951 年",
-        sales: 38
-      },
-      {
-        year: "1952 年",
-        sales: 52
-      },
-      {
-        year: "1956 年",
-        sales: 61
-      },
-      {
-        year: "1957 年",
-        sales: 145
-      },
-      {
-        year: "1958 年",
-        sales: 48
-      },
-      {
-        year: "1959 年",
-        sales: 38
-      },
-      {
-        year: "1960 年",
-        sales: 38
-      },
-      {
-        year: "1962 年",
-        sales: 38
-      }
-    ];
-    const cols = {
-      sales: {
-        tickInterval: 20
-      }
-    };
+    const {  }=this.state;
     return (
-      <div>
-      <Chart height={400} data={data} scale={cols} forceFit>
-        <Axis name="year" />
-        <Axis name="sales" />
-        <Tooltip
-          crosshairs={{
-            type: "y"
-          }}
-        />
-        <Geom type="interval" position="year*sales" />
-      </Chart>
-        <WordChart option={wordOption} config={config}/>
-        <BarChart option={barOption} config={config}/>
-        <br/>
-        <LineChart option={lineOption} config={config}/>
-        <br/>
-        <MapChart option={mapOption} conifg={config}/>
-        <br/>
-        <ScatterChart option={scatterOption} conifg={config}/>
-        <br/>
-        <PieChart option={pieOption} conifg={config}/>
+      <div className="home-page">
+        <div className="banner-part">
+          <img src={require('assets/imgs/fire.jpg')}></img>
+          <div className="welcome-box">
+            <h1>欢迎进入陌生人的小世界</h1>
+            <h3>遇见另一个自己</h3>
+            <Button className="btn" type="primary">探索更多</Button>
+          </div>
+        </div>
+        <div className="found-part">
+          <Divider className="head-big">寻</Divider>
+          <div className="head-small">人生就是在不停奔跑追寻着什么</div>
+          <Row gutter={16}>
+            <Col span={8}>
+              <div className="first-box">
+                <img src={require('assets/imgs/f4.jpg')} className="img-auto"></img>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="first-box">
+                <img src={require('assets/imgs/f2.jpg')} className="img-auto"></img>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="first-box">
+                <img src={require('assets/imgs/f3.jpg')} className="img-auto"></img>
+              </div>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <div className="second-box">
+                <img src={require('assets/imgs/f1.jpg')} className="img-auto"></img>
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className="second-box">
+                <img src={require('assets/imgs/f5.jpg')} className="img-auto"></img>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
