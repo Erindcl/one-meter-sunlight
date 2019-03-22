fe12fd0372fcf4a718dae621f7febfa9f9bc637f
Erindcl <cailian_erin@163.com>
2019-03-22 09:55:35 +0800

    完成基础设置模块开发

--- src/pages/BasicSetting/components/SettingsForm/SettingsForm.jsx
+++ src/pages/BasicSetting/components/SettingsForm/SettingsForm.jsx
@@ -1,43 +1,31 @@
 /* eslint  react/no-string-refs: 0 */
 import React, { Component } from 'react';
-import IceContainer from '@icedesign/container';
-import {
-  Input,
-  Button,
-  Radio,
-  Switch,
-  Upload,
-  Grid,
-  Message,
-} from '@alifd/next';
-import {
-  FormBinderWrapper as IceFormBinderWrapper,
-  FormBinder as IceFormBinder,
-  FormError as IceFormError,
-} from '@icedesign/form-binder';
+import { Card, Form, Input, Row, Col, Button, message, Radio, Upload, Icon } from 'antd';
 import './SettingsForm.scss';
 
-const { Row, Col } = Grid;
-const { Group: RadioGroup } = Radio;
+const RadioGroup = Radio.Group;
+const FormItem = Form.Item;
+const Dragger = Upload.Dragger;
 
-
-function beforeUpload(info) {
-  console.log('beforeUpload callback : ', info);
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
 }
 
-function onChange(info) {
-  console.log('onChane callback : ', info);
+function beforeUpload(file) {
+  const isJPG = file.type === 'image/jpeg';
+  if (!isJPG) {
+    message.error('You can only upload JPG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJPG && isLt2M;
 }
 
-function onSuccess(res, file) {
-  console.log('onSuccess callback : ', res, file);
-}
-
-function onError(file) {
-  console.log('onError callback : ', file);
-}
-
-export default class SettingsForm extends Component {
+class SettingsForm extends Component {
   static displayName = 'SettingsForm';
 
   static propTypes = {};
@@ -47,218 +35,196 @@ export default class SettingsForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: {
-        name: '',
-        gender: 'male',
-        notice: false,
-        email: '',
-        avatar: [],
-        siteUrl: '',
-        githubUrl: '',
-        twitterUrl: '',
-        description: '',
-      },
+      loading: false,
     };
   }
 
-  onDragOver = () => {
-    console.log('dragover callback');
-  };
+  handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, imageUrl => this.setState({
+        imageUrl,
+        loading: false,
+      }));
+    }
+  }
 
-  onDrop = (fileList) => {
-    console.log('drop callback : ', fileList);
-  };
-
-  formChange = (value) => {
-    console.log('value', value);
-  };
-
-  validateAllFormField = () => {
-    this.refs.form.validateAll((errors, values) => {
+  handleSubmit = () => {
+    this.props.form.validateFields((errors, values) => {
       if (errors) {
         return;
       }
       console.log(values);
-      Message.success('提交成功');
+      message.success('提交成功');
     });
   };
 
   render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span: 11,
+      },
+    };
+
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const imageUrl = this.state.imageUrl;
+    
     return (
       <div className="settings-form">
-        <IceContainer>
-          <IceFormBinderWrapper
-            value={this.state.value}
-            onChange={this.formChange}
-            ref="form"
-          >
+        <Card>
+          <Form>
             <div style={styles.formContent}>
               <h2 style={styles.formTitle}>基本设置</h2>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  姓名：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder name="name" required max={10} message="必填">
-                    <Input placeholder="于江水" />
-                  </IceFormBinder>
-                  <IceFormError name="name" />
+                <Col span="15">
+                  <FormItem label="姓名：" {...formItemLayout}>
+                    {getFieldDecorator('name', {
+                      rules: [{
+                        required: true, message: '必填项',
+                      }],
+                    })(
+                      <Input size="large" placeholder="请输入用户名" />
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  头像：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder name="avatar" required message="必填">
-                    <Upload.Card
-                      listType="card"
-                      action=""
-                      accept="image/png, image/jpg, image/jpeg, image/gif, image/bmp"
+                <Col span="15">
+                  <FormItem label="姓名：" {...formItemLayout}>
+                    <Upload
+                      name="avatar"
+                      listType="picture-card"
+                      className="avatar-uploader"
+                      showUploadList={false}
+                      action="//jsonplaceholder.typicode.com/posts/"
                       beforeUpload={beforeUpload}
-                      onChange={onChange}
-                      onSuccess={onSuccess}
-                      onError={onError}
-                    />
-                  </IceFormBinder>
-                  <IceFormError name="avatar" />
+                      onChange={this.handleChange}
+                    >
+                      {imageUrl ? <img src={imageUrl} alt="avatar" /> : uploadButton}
+                    </Upload>
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  性别：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder name="gender" required message="必填">
-                    <RadioGroup>
-                      <Radio value="male">男</Radio>
-                      <Radio value="female">女</Radio>
-                    </RadioGroup>
-                  </IceFormBinder>
-                  <IceFormError name="gender" />
-                </Col>
-              </Row>
-
-              <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  通知：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder type="boolean" name="notice">
-                    <Switch />
-                  </IceFormBinder>
-                  <IceFormError name="notice" />
+                <Col span="15">
+                  <FormItem label="性别：" {...formItemLayout}>
+                    {getFieldDecorator('gender', {
+                      rules: [{
+                        required: true, message: '必填项',
+                      }],
+                    })(
+                      <RadioGroup>
+                        <Radio value={'male'}>男</Radio>
+                        <Radio value={'female'}>女</Radio>
+                      </RadioGroup>
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  邮件：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder
-                    type="email"
-                    name="email"
-                    required
-                    message="请输入正确的邮件"
-                  >
-                    <Input
-                      placeholder="ice-admin@alibaba-inc.com"
-                    />
-                  </IceFormBinder>
-                  <IceFormError name="email" />
+                <Col span="15">
+                  <FormItem label="邮箱：" {...formItemLayout}>
+                    {getFieldDecorator('email', {
+                      rules: [{
+                        required: true, message: '请输入正确的邮箱', type: 'email'
+                      }],
+                    })(
+                      <Input size="large" placeholder="请输入邮箱" />
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  website ：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder
-                    type="url"
-                    name="siteUrl"
-                    required
-                    message="请输入正确的网站地址"
-                  >
-                    <Input
-                      type="url"
-                      placeholder="https://alibaba.github.io/ice"
-                    />
-                  </IceFormBinder>
-                  <IceFormError name="siteUrl" />
+                <Col span="15">
+                  <FormItem label="website：" {...formItemLayout}>
+                    {getFieldDecorator('siteUrl', {
+                      rules: [{
+                        required: true, message: '请输入正确的网站地址', type: 'url'
+                      }],
+                    })(
+                      <Input size="large" placeholder="请输入网站地址" />
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  Github：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder
-                    type="url"
-                    name="githubUrl"
-                    required
-                    message="请输入正确的 Github 地址"
-                  >
-                    <Input
-                      placeholder="https://github.com/alibaba/ice"
-                    />
-                  </IceFormBinder>
-                  <IceFormError name="githubUrl" />
+                <Col span="15">
+                  <FormItem label="Github：" {...formItemLayout}>
+                    {getFieldDecorator('githubUrl', {
+                      rules: [{
+                        required: true, message: '请输入正确的 Github 地址', type: 'url'
+                      }],
+                    })(
+                      <Input size="large" placeholder="请输入Github地址" />
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  Twitter：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder
-                    type="url"
-                    name="twitterUrl"
-                    required
-                    message="请输入正确的 Twitter 地址"
-                  >
-                    <Input placeholder="https://twitter.com" />
-                  </IceFormBinder>
-                  <IceFormError name="twitterUrl" />
+                <Col span="15">
+                  <FormItem label="Twitter：" {...formItemLayout}>
+                    {getFieldDecorator('twitterUrl', {
+                      rules: [{
+                        required: true, message: '请输入正确的 Twitter 地址', type: 'url'
+                      }],
+                    })(
+                      <Input size="large" placeholder="请输入Twitter地址" />
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
 
               <Row style={styles.formItem}>
-                <Col xxs="6" s="4" l="3" style={styles.label}>
-                  自我描述：
-                </Col>
-                <Col xxs="16" s="10" l="6">
-                  <IceFormBinder name="description">
-                    <Input.TextArea placeholder="请输入描述..." />
-                  </IceFormBinder>
-                  <IceFormError name="description" />
+                <Col span="15">
+                  <FormItem label="自我描述：" {...formItemLayout}>
+                    {getFieldDecorator('description', {})(
+                      <Input.TextArea placeholder="请输入自我描述">
+                      </Input.TextArea>
+                    )}
+                  </FormItem>
                 </Col>
               </Row>
             </div>
-          </IceFormBinderWrapper>
+          </Form>
 
           <Row style={{ marginTop: 20 }}>
-            <Col offset="3">
+            <Col offset="1">
               <Button
                 type="primary"
                 style={{ width: 100 }}
-                onClick={this.validateAllFormField}
+                onClick={this.handleSubmit}
               >
                 提 交
               </Button>
             </Col>
           </Row>
-        </IceContainer>
+        </Card>
       </div>
     );
   }
 }
+
+export default Form.create()(SettingsForm);
 
 const styles = {
   label: {
