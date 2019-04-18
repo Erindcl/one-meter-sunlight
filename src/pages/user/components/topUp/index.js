e236511748f12fbce5f9a289b483819bbc10fb32
Erindcl <cailian_erin@163.com>
2019-04-18 09:24:19 +0800

    添加充值界面逻辑

--- src/pages/user/components/topUp/index.js
+++ src/pages/user/components/topUp/index.js
@@ -3,12 +3,20 @@ import "./style.scss";
 import { Row, Col, Card, Button, Modal } from 'antd';
 
 const confirm = Modal.confirm;
+const option = [
+  { corn: 20, rmb: 1 },
+  { corn: 100, rmb: 5 },
+  { corn: 210, rmb: 10 },
+  { corn: 350, rmb: 15 },
+  { corn: 500, rmb: 20 },
+  { corn: 1000, rmb: 30 },
+]
 
 export default class TopUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      selectedOne: 1
     };
   }
   componentDidMount() {
@@ -16,13 +24,14 @@ export default class TopUp extends Component {
   }
 
   showConfirm = () => {
+    let currentItem = option[this.state.selectedOne];
     confirm({
-      title: '确认购买米币30?',
-      content: '需要您支付60RMB',
+      title: `确认购买 米币${currentItem.corn}?`,
+      content: `需要您支付 ${currentItem.rmb} RMB`,
       okText: '确认',
       cancelText: '取消',
       onOk() {
-        
+        // 请求购买米币
       },
       onCancel() {
         
@@ -30,53 +39,57 @@ export default class TopUp extends Component {
     });
   }
 
+  handleClick = (index) => {
+    this.setState({ selectedOne: index });
+  }
+
   render() {
-    const {  }=this.state;
+    const { selectedOne }=this.state;
     return (
       <div className="top-up">
         <Row gutter={16}>
           <Col span={8}>
-            <Card className="card-box" style={{ border: '1px solid #F05133' }} bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'block' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,1)} className="card-box" style={selectedOne == 1 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">20</div>
+              <div className="small">售价：1RMB</div>
+              <div className="checked" style={{ display: selectedOne == 1 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
           <Col span={8}>
-            <Card className="card-box" bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'none' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,2)} className="card-box" style={selectedOne == 2 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">100</div>
+              <div className="small">售价：5RMB</div>
+              <div className="checked" style={{ display: selectedOne == 2 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
           <Col span={8}>
-            <Card className="card-box" bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'none' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,3)} className="card-box" style={selectedOne == 3 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">210</div>
+              <div className="small">售价：10RMB</div>
+              <div className="checked" style={{ display: selectedOne == 3 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
         </Row>
         <Row gutter={16} style={{ marginTop: '20px' }}>
           <Col span={8}>
-            <Card className="card-box" bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'none' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,4)} className="card-box" style={selectedOne == 4 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">350</div>
+              <div className="small">售价：15RMB</div>
+              <div className="checked" style={{ display: selectedOne == 4 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
           <Col span={8}>
-            <Card className="card-box" bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'none' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,5)} className="card-box" style={selectedOne == 5 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">500</div>
+              <div className="small">售价：20RMB</div>
+              <div className="checked" style={{ display: selectedOne == 5 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
           <Col span={8}>
-            <Card className="card-box" bordered={true}>
-              <div className="large">30</div>
-              <div className="small">售价：60RMB</div>
-              <div className="checked" style={{ display: 'none' }}>√</div>
+            <Card onClick={this.handleClick.bind(this,6)} className="card-box" style={selectedOne == 6 ? { border: '1px solid #F05133' } : {}} bordered={true}>
+              <div className="large">1000</div>
+              <div className="small">售价：30RMB</div>
+              <div className="checked" style={{ display: selectedOne == 6 ? 'block' : 'none' }}>√</div>
             </Card>
           </Col>
         </Row>
