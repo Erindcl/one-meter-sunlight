aa688ad5fffd6411f70eb1fe70b7e36c93c2d3d6
Erindcl <cailian_erin@163.com>
2019-04-18 09:45:51 +0800

    添加历史评论模块布局

--- src/pages/user/components/historyRemark/index.js
+++ src/pages/user/components/historyRemark/index.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 import "./style.scss";
+import { Timeline, Icon } from 'antd';
 
 export default class HistoryRemark extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      remarks: [
+        { date: '2019-10-15 11:15:45', content: '这个故事太温馨了太温馨了太温馨了太温馨了太温馨了太温馨了太温馨了太温馨了太温馨了太温馨了，超级感动！', story: 'xxxxx' },
+        { date: '2019-10-15 11:15:45', content: '这个故事太温馨了，超级感动！', story: 'xxxxx' },
+        { date: '2019-10-15 11:15:45', content: '这个故事太温馨了，超级感动！', story: 'xxxxx' },
+        { date: '2019-10-15 11:15:45', content: '这个故事太温馨了，超级感动！', story: 'xxxxx' },
+        { date: '2019-10-15 11:15:45', content: '这个故事太温馨了，超级感动！', story: 'xxxxx' },
+      ]
     };
   }
   componentDidMount() {
     
   }
   render() {
-    const {  }=this.state;
+    const { remarks }=this.state;
     return (
       <div className="history-remark">
-        历史评论
+        <Timeline reverse={false}>
+          <Timeline.Item dot={<Icon type="loading" style={{ fontSize: '16px' }} />}>
+              <div className="date">更多评论等待你的添加...</div>
+              {/* <div className="content">{item.content}</div> */}
+          </Timeline.Item>
+          {remarks.map((item,index) => (
+            <Timeline.Item dot={<Icon type="check-circle" style={{ fontSize: '16px' }} />} key={index}>
+              <div className="date">您于 {item.date} 对 {item.story} 评论到</div>
+              <div className="content">{item.content}</div>
+            </Timeline.Item>
+          ))}
+        </Timeline>
       </div>
     );
   }
