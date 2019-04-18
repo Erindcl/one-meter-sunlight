c96c2f5dc3630bd213f71a6187a607883b3f10ab
Erindcl <cailian_erin@163.com>
2019-04-18 17:06:01 +0800

    添加历史订单界面布局

--- src/pages/user/components/paiedStory/index.js
+++ src/pages/user/components/paiedStory/index.js
@@ -1,21 +1,96 @@
 import React, { Component } from "react";
 import "./style.scss";
+import { Card, Pagination } from 'antd';
 
 export default class PaiedStory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      pageNo: 1,
+      total: 22,
+      pageSize: 20
     };
   }
   componentDidMount() {
     
   }
+
+  handlePageChange = (page, pageSize) => {
+    this.setState({ pageNo: page }, () => {
+      // this.getStoryList();
+    })
+  }
+
   render() {
-    const {  }=this.state;
+    const { pageNo, total, pageSize }=this.state;
     return (
       <div className="paied-story">
-        已购故事
+        <Card
+          type="inner"
+          title="2019-04-04 12:15:45"
+          extra={<a href="#">查看</a>}
+        >
+          <div className="story-item-box">
+            <div className="img-box">
+              <img src={require('assets/imgs/t6.jpg')}></img>
+            </div>
+            <div className="infor">
+              <div className="title">咖啡店那一角的风景</div>
+              <div className="corn">米币： 30</div>
+            </div>
+          </div>
+        </Card>
+        <Card
+          style={{ marginTop: 16 }}
+          type="inner"
+          title="2019-04-04 12:15:45"
+          extra={<a href="#">查看</a>}
+        >
+          <div className="story-item-box">
+            <div className="img-box">
+              <img src={require('assets/imgs/t3.jpg')}></img>
+            </div>
+            <div className="infor">
+              <div className="title">咖啡店那一角的风景</div>
+              <div className="corn">米币： 30</div>
+            </div>
+          </div>
+        </Card>
+        <Card
+          style={{ marginTop: 16 }}
+          type="inner"
+          title="2019-04-04 12:15:45"
+          extra={<a href="#">查看</a>}
+        >
+          <div className="story-item-box">
+            <div className="img-box">
+              <img src={require('assets/imgs/t1.jpg')}></img>
+            </div>
+            <div className="infor">
+              <div className="title">咖啡店那一角的风景</div>
+              <div className="corn">米币： 30</div>
+            </div>
+          </div>
+        </Card>
+        <Card
+          style={{ marginTop: 16 }}
+          type="inner"
+          title="2019-04-04 12:15:45"
+          extra={<a href="#">查看</a>}
+        >
+          <div className="story-item-box">
+            <div className="img-box">
+              <img src={require('assets/imgs/t4.jpg')}></img>
+            </div>
+            <div className="infor">
+              <div className="title">咖啡店那一角的风景</div>
+              <div className="corn">米币： 30</div>
+            </div>
+          </div>
+        </Card>
+        <dvi className="pagination-box">
+          <Pagination current={pageNo} onChange={this.handlePageChange} total={total} pageSize={pageSize} />
+        </dvi>
       </div>
     );
   }
