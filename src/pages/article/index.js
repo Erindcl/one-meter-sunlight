0ba7c12b260cae61c4559e6e5cc903c1b4877f17
Erindcl <cailian_erin@163.com>
2019-04-28 17:53:05 +0800

    添加请求：获取评论

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Divider, Comment, Tooltip, List, Pagination } from 'antd';
+import { Button, Divider, Comment, Empty, List, Pagination } from 'antd';
 import moment from 'moment';
 import { API } from "@/api/index.js";
 import { message as Message } from 'antd';
@@ -9,34 +9,9 @@ export default class Article extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgsrc: require('assets/imgs/t4.jpg'),
-      remarkData: [
-        {
-          author: 'Han Solo',
-          avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-          content: (
-            <p>We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.</p>
-          ),
-          datetime: (
-            <Tooltip title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().subtract(1, 'days').fromNow()}</span>
-            </Tooltip>
-          ),
-        },
-        {
-          author: 'Han Solo',
-          avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-          content: (
-            <p>We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.</p>
-          ),
-          datetime: (
-            <Tooltip title={moment().subtract(2, 'days').format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().subtract(2, 'days').fromNow()}</span>
-            </Tooltip>
-          ),
-        },
-      ],
+      remarkData: [],
       detailInfor: {},
+      total: 0
     };
   }
   componentDidMount() {
@@ -54,22 +29,42 @@ export default class Article extends Component {
       if (success) {
         this.setState({
           detailInfor: data || {}
-        })
+        });
+        if (data.remarkGroup) {
+          this.getRemarkList(data.remarkGroup);
+        }
       } else {
         Message.error(message);
       }
     });
   }
 
+  getRemarkList = (ids) => {
+    API.getRemarkList({
+      id: ids, 
+      pageSize: 1, 
+      pageNo: 1
+    }).then(response =>{ 
+      const { success, message, data } = response;
+      if (success) {
+        this.setState({ 
+          remarkData: data.list || [],
+          total: data.total || 0
+        });
+      } else {
+        Message.error(message); 
+      }
+    });
+  }
+
   render() {
-    const { imgsrc, remarkData, detailInfor } = this.state;
+    const { remarkData, detailInfor, total } = this.state;
     let realImgSrc = detailInfor.coverPic ? require(`assets/imgs/article/${detailInfor.coverPic}`) : '';
     let contentArray = detailInfor.contentPart ? detailInfor.contentPart.split('\n') : [];
     return (
       <div className="article-container">
         <div className="cover-img" style={{ backgroundImage: `url(${realImgSrc})`, 
         backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundAttachment: 'fixed'}}></div>
-        {/* <img src={realImgSrc} className="cover-img"></img> */}
         <div className="cover-mask">
           <div className="artcle-infor">
             <div className="title">{detailInfor.title}</div>
@@ -99,17 +94,19 @@ export default class Article extends Component {
           <List
             className="comment-list"
             itemLayout="horizontal"
-            dataSource={remarkData}
-            renderItem={item => (
-              <Comment
-                author={item.author}
-                avatar={item.avatar}
-                content={item.content}
-                datetime={item.datetime}
-              />
-            )}
+            dataSource={remarkData.length > 0 ?remarkData : [1]}
+            renderItem={(item) => {
+              return remarkData.length > 0 ?  (
+                <Comment
+                  author={item.author}
+                  avatar={item.avatar}
+                  content={item.content}
+                  datetime={item.datetime}
+                />
+              ) : <Empty description={'暂无评论'} />;
+            }}
           />
-          <Pagination defaultCurrent={1} total={10} />
+          <Pagination style={{ marginTop: 20 }} defaultCurrent={1} total={total} />
         </div>
       </div>
     );
